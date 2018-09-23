f239045d5a962b197f038a324e8fa1645aad7e28
Elijah Liedtke <chimera.zen@gmail.com>
2018-09-22 20:32:33 -0400

    LinearMeter v1.0.0
    A simple meter that fills up to an indicated percentage

---
+++ src/components/Meters/Linear/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+// Styles
+import './styles.css'
+
+export default class LinearMeter extends React.Component {
+  state = {
+    barWidth: 0,
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer, 10)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
+  timer = () => {
+    if(this.state.barWidth < this.props.percent) {
+      const barWidth = this.state.barWidth + 1
+      this.setState({
+        barWidth: barWidth
+      })
+    }
+  }
+
+  render() {
+    const style = {
+      width: this.state.barWidth + "%",
+    }
+    
+    return (
+      <div className="linear-meter">
+        <div className="linear-meter-header">
+          <h4>{this.props.title}</h4>
+          <span>{this.state.barWidth + "%"}</span>
+        </div>
+        <div className="linear-meter-bar">
+          <span style={style}></span>
+        </div>
+      </div>
+    )
+  }
+}
